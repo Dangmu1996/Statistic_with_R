dd3b07ea268083a1fa4d5c99da6ddf5dc6b10b69
dangmu1996 <changmin5941@gmail.com>
2023-09-08 01:07:44 +0900

    data_type

---
+++ chapter_1/data_type.R
@@ -0,0 +1,23 @@
+# 논리형  자료형
+(a = c(T, F, F, T))
+typeof(a)
+
+#정수형
+(b = 1:5)
+typeof(b)
+
+#실수형
+(c = c(1.2, 3.45))
+typeof(c)
+
+#복소수
+(d=c(1+2i, 3+4i))
+typeof(d)
+
+#문자형
+(e=LETTERS[1:4])
+typeof(e)
+
+#요인형 : 구룹을 구분하는 요인 수준의 번호
+(f=as.factor(e))
+typeof(f)
